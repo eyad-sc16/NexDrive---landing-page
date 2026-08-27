f36636da14abfc03f4129116cdff14d3eb022dcd
Eyad nasser <eyadnasser499@gmail.com>
2026-08-27 17:45:00 +0300

    implementing the slider

--- script.js
+++ script.js
@@ -69,3 +69,101 @@ innerMobileMenueNav.addEventListener('click', function (e) {
 logos.forEach(logo => logo.addEventListener('click', function (e) {
   closeFn()
 }))
+
+
+/// Slider
+
+const slider = function () {
+  // Data Selecting
+  const sliderSection = document.querySelector('.slider-section  .container')
+  console.log(sliderSection);
+  const sliders = document.querySelectorAll('.slide')
+  const rightBtn = document.querySelector('.slider__arrow--next')
+  const leftBtn = document.querySelector('.slider__arrow--prev')
+  const dots = document.querySelector('.slider__dots')
+  console.log(sliders);
+  let currentSlide = 0
+  const maxSlider = sliders.length
+
+  // Functions
+  const creatDots = function () {
+
+    sliders.forEach((_, i) => {
+      const html = `<botton class="slider__dot" data-slide="${i}"> </button>`
+      dots.insertAdjacentHTML('beforeend', html)
+    })
+    console.log(dots);
+  }
+
+  const activeDot = function (slide) {
+    document.querySelectorAll('.slider__dot').forEach(dot => dot.classList.remove('is-active'))
+    document.querySelector(`.slider__dot[data-slide="${slide}"]`).classList.add('is-active')
+  }
+
+  const goToSlide = function (slide) {
+    sliders.forEach((s, i) => s.style.transform = `translatex(${(i - slide) * 100}%)`)
+  }
+
+
+
+  const toNextSlide = function () {
+    if (currentSlide === maxSlider - 1) {
+      currentSlide = 0
+    } else {
+      currentSlide++
+    }
+    goToSlide(currentSlide)
+    activeDot(currentSlide)
+  }
+
+  const toPrevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlider - 1
+    } else {
+      currentSlide--
+    }
+    goToSlide(currentSlide)
+    activeDot(currentSlide)
+  }
+
+  const init = function (slide) {
+    creatDots()
+    activeDot(slide)
+    goToSlide(slide)
+  }
+  init(currentSlide)
+
+  // event listeners
+  dots.addEventListener('click', function (e) {
+    currentSlide = +e.target.dataset.slide
+    if (e.target.classList.contains('slider__dot')) {
+      goToSlide(currentSlide)
+      activeDot(currentSlide)
+    }
+    // console.log('DOT');
+
+  })
+  rightBtn.addEventListener('click', function () {
+    toNextSlide()
+    goToSlide(currentSlide)
+  })
+
+  leftBtn.addEventListener('click', function () {
+    toPrevSlide()
+  })
+
+
+  const keyEvents = function (entries) {
+    const [entry] = entries
+
+    if (!entry.isIntersecting) return
+
+    document.addEventListener('keydown', function (e) {
+      e.key === 'ArrowRight' && toNextSlide()
+      e.key === 'ArrowLeft' && toPrevSlide()
+    })
+  }
+  new IntersectionObserver(keyEvents, { root: null, threshold: 0.20 }).observe(sliderSection)
+}
+
+slider()
